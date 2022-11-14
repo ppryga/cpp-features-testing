1645cd356675c14c2fb25060ea83716d39090807
Piotr Pryga <piotr.pryga@nordicsemi.no>
2022-11-14 23:55:46 +0100

    patterns: remove the directory and its contents from repository
    Contents of this subdirectory was added to repository by accident.
    This commit cleans this mess.
    
    Signed-off-by: Piotr Pryga <pryga.piotr@outlook.com>

--- patterns/builder/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/piotr/projects/test-cpp/patterns/builder
-# Build directory: /home/piotr/projects/test-cpp/patterns/builder/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
